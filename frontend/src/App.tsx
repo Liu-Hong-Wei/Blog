be50ad89412980a6c02afa77f436781b30989f2d
Homey Lau <lhw1953927853@gmail.com>
2025-03-26 00:21:10 +0800

    fix(backend): modify index page and facicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Once upon there was a explosion...</h1>
+      <h1>Once there was an explosion...</h1>
+      <img src="https://m.media-amazon.com/images/M/MV5BZTkyMWZjMmQtYzczZC00MmQxLTg0YzctMDAwMDYyMTAxNmEwXkEyXkFqcGc@._V1_.jpg" alt="Movie The Dreamers" style={{ width: '80%' }}/>
+      <p>U know what, <a href="https://teahour.dev">teahour</a> is the best Chinese podcast I've listened. </p>
     </>
   )
 }
