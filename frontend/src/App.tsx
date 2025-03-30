edf3daa48b0d36893a8fac0731ea08ed0dd6d5d8
Homey Lau <lhw1953927853@gmail.com>
2025-03-30 16:33:50 +0800

    feat(frontend): add navbar layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
-import './App.css'
+import Navbar from "./layouts/Navbar";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <>
-      <h1>Once there was an explosion...</h1>
-      <img src="https://m.media-amazon.com/images/M/MV5BZTkyMWZjMmQtYzczZC00MmQxLTg0YzctMDAwMDYyMTAxNmEwXkEyXkFqcGc@._V1_.jpg" alt="Movie The Dreamers" style={{ width: '80%' }}/>
-      <p>U know what, <a href="https://teahour.dev">teahour</a> is the best Chinese podcast I've listened. </p>
+      <div className="font-serif">
+        <Navbar>
+          <Homepage />{" "}
+        </Navbar>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,27 @@
+function Homepage() {
+  return (
+    <>
+      <div>
+      <main>
+        <img
+          src="https://m.media-amazon.com/images/M/MV5BZTkyMWZjMmQtYzczZC00MmQxLTg0YzctMDAwMDYyMTAxNmEwXkEyXkFqcGc@._V1_.jpg"
+          alt="Movie The Dreamers"
+          style={{ width: "300px" }}
+        />
+        <p className="">
+          U know what, <a href="https://teahour.dev">teahour</a> is the best
+          Chinese podcast I've listened.{" "}
+        </p>
+      </main>
+      <aside>
+        <img
+          src="https://m.media-amazon.com/images/M/MV5BZTkyMWZjMmQtYzczZC00MmQxLTg0YzctMDAwMDYyMTAxNmEwXkEyXkFqcGc@._V1_.jpg"
+          alt="Movie The Dreamers"
+          style={{ width: "300px" }}
+        />
+      </aside>
+      </div>
+    </>
+  );
+}
+export default Homepage;
