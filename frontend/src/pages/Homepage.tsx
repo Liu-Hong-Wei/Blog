5e46a4a8c368f5b368a0ce6c5d819577fa90f3d7
Homey Lau <lhw1953927853@gmail.com>
2025-04-01 21:44:04 +0800

    feat(frontend): add anti-baidu script and delete example text

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,8 +1,8 @@
 function Homepage() {
   return (
     <>
-      <div className="w-screen min-h-full flex justify-center">
-        <section className="w-full max-w-[1200px] flex flex-col md:flex-row min-h-screen">
+      <div className="w-full min-h-full flex justify-center">
+        <section className="w-full max-w-[1200px] flex flex-col md:flex-row min-h-full">
           <main className="w-full md:w-[742px] p-4 bg-amber-50">
             <h1 className="mb-4">
               U know what,{" "}
@@ -14,142 +14,19 @@ function Homepage() {
               </a>{" "}
               is the best Chinese podcast I've listened.
             </h1>
-            So she was considering in her own mind (as well as she could, for
-            the day made her feel very sleepy and stupid), whether the pleasure
-            of making a daisy-chain would be worth the trouble of getting up and
-            picking the daisies, when suddenly a White Rabbit with pink eyes ran
-            close by her. There was nothing so very remarkable in that, nor did
-            Alice think it so very much out of the way to hear the Rabbit say to
-            itself, "Oh dear! Oh dear! I shall be too late!" But when the Rabbit
-            actually took a watch out of its waistcoat-pocket and looked at it
-            and then hurried on, Alice started to her feet, for it flashed
-            across her mind that she had never before seen a rabbit with either
-            a waistcoat-pocket, or a watch to take out of it, and, burning with
-            curiosity, she ran across the field after it and was just in time to
-            see it pop down a large rabbit-hole, under the hedge. In another
-            moment, down went Alice after it! [Illustration] The rabbit-hole
-            went straight on like a tunnel for some way and then dipped suddenly
-            down, so suddenly that Alice had not a moment to think about
-            stopping herself before she found herself falling down what seemed
-            to be a very deep well.
             <img
               src="https://m.media-amazon.com/images/M/MV5BZTkyMWZjMmQtYzczZC00MmQxLTg0YzctMDAwMDYyMTAxNmEwXkEyXkFqcGc@._V1_.jpg"
               alt="Movie The Dreamers"
               className="w-full max-w-md mx-auto my-4"
             />
-            Either the well was very deep, or she fell very slowly, for she had
-            plenty of time, as she went down, to look about her. First, she
-            tried to make out what she was coming to, but it was too dark to see
-            anything; then she looked at the sides of the well and noticed that
-            they were filled with cupboards and book-shelves; here and there she
-            saw maps and pictures hung upon pegs. She took down a jar from one
-            of the shelves as she passed. It was labeled "ORANGE MARMALADE,"
-            but, to her great disappointment, it was empty; she did not like to
-            drop the jar, so managed to put it into one of the cupboards as she
-            fell past it. Down, down, down! Would the fall never come to an end?
-            There was nothing else to do, so Alice soon began talking to
-            herself. "Dinah'll miss me very much to-night, I should think!"
-            (Dinah was the cat.) "I hope they'll remember her saucer of milk at
-            tea-time. Dinah, my dear, I wish you were down here with me!" Alice
-            felt that she was dozing off, when suddenly, thump! thump! down she
-            came upon a heap of sticks and dry leaves, and the fall was over.
-            Alice was not a bit hurt, and she jumped up in a moment. She looked
-            up, but it was all dark overhead; before her was another long
-            passage and the White Rabbit was still in sight, hurrying down it.
-            There was not a moment to be lost. Away went Alice like the wind and
-            was just in time to hear it say, as it turned a corner, "Oh, my ears
-            and whiskers, how late it's getting!" She was close behind it when
-            she turned the corner, but the Rabbit was no longer to be seen. She
-            found herself in a long, low hall, which was lit up by a row of
-            lamps hanging from the roof. There were doors all 'round the hall,
-            but they were all locked; and when Alice had been all the way down
-            one side and up the other, trying every door, she walked sadly down
-            the middle, wondering how she was ever to get out again. Suddenly
-            she came upon a little table, all made of solid glass. There was
-            nothing on it but a tiny golden key, and Alice's first idea was that
-            this might belong to one of the doors of the hall; but, alas! either
-            the locks were too large, or the key was too small, but, at any
-            rate, it would not open any of them. However, on the second time
-            'round, she came upon a low curtain she had not noticed before, and
-            behind it was a little door about fifteen inches high. She tried the
-            little golden key in the lock, and to her great delight, it fitted!
-            [Illustration] Alice opened the door and found that it led into a
-            small passage, not much larger than a rat-hole; she knelt down and
-            looked along the passage into the loveliest garden you ever saw. How
-            she longed to get out of that dark hall and wander about among those
-            beds of bright flowers and those cool fountains, but she could not
-            even get her head through the doorway. "Oh," said Alice, "how I wish
-            I could shut up like a telescope! I think I could, if I only knew
-            how to begin." Alice went back to the table, half hoping she might
-            find another key on it, or at any rate, a book of rules for shutting
-            people up like telescopes. This time she found a little bottle on it
-            ("which certainly was not here before," said Alice), and tied 'round
-            the neck of the bottle was a paper label, with the words "DRINK ME"
-            beautifully printed on it in large letters.
           </main>
           <aside className="w-full md:w-[458px] p-4 bg-yellow-200">
-            <h2 className="mb-4 font-bold">Teahour Highlights</h2>
-            <p className="mb-4">
-              Teahour is the best OTHER WARRANTIES OF ANY KIND, EXPRESS OR
-              IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
-              MERCHANTABILITY OR FITNESS FOR ANY PURPOSE. 1.F.5. Some states do
-              not allow disclaimers of certain implied warranties or the
-              exclusion or limitation of certain types of damages. If any
-              disclaimer or limitation set forth in this agreement violates the
-              law of the state applicable to this agreement, the agreement shall
-              be interpreted to make the maximum disclaimer or limitation
-              permitted by the applicable state law. The invalidity or
-              unenforceability of any provision of this agreement shall not void
-              the remaining provisions. 1.F.6. INDEMNITY - You agree to
-              indemnify and hold the Foundation, the trademark owner, any agent
-              or employee of the Foundation, anyone providing copies of Project
-              Gutenberg™ electronic works in accordance with this agreement, and
-              any volunteers associated with the production, promotion and
-              distribution of Project Gutenberg™ electronic works, harmless from
-              all liability, costs and expenses, including legal fees, that
-              arise directly or indirectly from any of the following which you
-              do or cause to occur: (a) distribution of this or any Project
-              Gutenberg™ work, (b) alteration, modification, or additions or
-              deletions to any Project Gutenberg™ work, and (c) any Defect you
-              cause. Section 2. Information about the Mission of Project
-              Gutenberg™ Project Gutenberg™ is synonymous with the free
-              distribution of electronic works in formats readable by the widest
-              variety of computers including obsolete, old, middle-aged and new
-              computers. It exists because of the efforts of hundreds of
-              volunteers and donations from people in all walks of life.
-              Volunteers and financial support to provide volunteers with the
+            <p>
               <img
                 src="https://m.media-amazon.com/images/M/MV5BZTkyMWZjMmQtYzczZC00MmQxLTg0YzctMDAwMDYyMTAxNmEwXkEyXkFqcGc@._V1_.jpg"
                 alt="Movie The Dreamers"
                 className="w-full max-w-[300px] mx-auto"
               />
-              assistance they need are critical to reaching Project Gutenberg™’s
-              goals and ensuring that the Project Gutenberg™ collection will
-              remain freely available for generations to come. In 2001, the
-              Project Gutenberg Literary Archive Foundation was created to
-              provide a secure and permanent future for Project Gutenberg™ and
-              future generations. To learn more about the Project Gutenberg
-              Literary Archive Foundation and how your efforts and donations can
-              help, see Sections 3 and 4 and the Foundation information page at
-              www.gutenberg.org. Section 3. Information about the Project
-              Gutenberg Literary Archive Foundation The Project Gutenberg
-              Literary Archive Foundation is a non-profit 501(c)(3) educational
-              corporation organized under the laws of the state of Mississippi
-              and granted tax exempt status by the Internal Revenue Service. The
-              Foundation’s EIN or federal tax identification number is
-              64-6221541. Contributions to the Project Gutenberg Literary
-              Archive Foundation are tax deductible to the full extent permitted
-              by U.S. federal laws and your state’s laws. The Foundation’s
-              business office is located at 809 North 1500 West, Salt Lake City,
-              UT 84116, (801) 596-1887. Email contact links and up to date
-              contact information can be found at the Foundation’s website and
-              official page at www.gutenberg.org/contact Section 4. Information
-              about Donations to the Project Gutenberg Literary Archive
-              Foundation Project Gutenberg™ depends upon and cannot survive
-              without widespread public support and donations to carry out its
-              mission of increasing the number of public domain and licensed
-              works that can be freely distributed in machine-readable form
-              accessible by the widest array of equipment including outdated
             </p>
           </aside>
         </section>
